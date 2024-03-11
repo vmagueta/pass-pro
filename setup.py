3c2849f9535444898adc7172939f32795f3dc718
vmagueta <solermvictor@gmail.com>
2024-03-10 22:57:25 -0300

    Add setup.py and turned the project into executable

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="passpro",
+    version="0.1.0",
+    description="Password manager application",
+    author="Victor Magueta",
+    packages=find_packages(),
+)
